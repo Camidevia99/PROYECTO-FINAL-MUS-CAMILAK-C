ad675a5bf5979b08bc2a5c773a5175a1ddd3ead1
camidevia99 <camidevia9901@hotmail.com>
2026-03-14 23:25:08 +0100

    Fix import in client_collection

--- src/client_collection.py
+++ src/client_collection.py
@@ -1,44 +1,34 @@
 import json
 from pathlib import Path
+from client import Client  
 
-# obtener la carpeta donde está este archivo
-base_path = Path(__file__).resolve().parent
 
-# ir a la carpeta data
-file_path = base_path.parent / "data" / "clients.json"
+class ClientCollection:
 
-# abrir el json
-with open(file_path, "r") as file:
-    clients = json.load(file)
+    def __init__(self):
 
-class Get:
+        base_path = Path(__file__).resolve().parent
+        file_path = base_path.parent / "data" / "clients.json"
 
-    def get_client_by_id(self, id):
-        for client in clients:
-            if client["client_id"] == id:
+        with open(file_path, "r", encoding="utf-8") as file:
+            data = json.load(file)
+
+        self.clients = [Client(**client) for client in data]
+
+
+    def count_clients(self):
+        return len(self.clients)
+
+
+    def get_client_by_id(self, client_id):
+
+        for client in self.clients:
+            if client.client_id == client_id:
                 return client
 
         return None
 
-# probar la función
-g = Get()
-client = g.get_client_by_id(2)
 
-print(client)
+    def clients_by_country(self, country):
 
-#
-class Country():
-
-    def clients_by_country(self,country):
-        result=[]
-        for client in clients:
-            if client["country"]==country:
-                result.append(client)
-        return result
-    
-# probar la función
-
-g = Country()
-clientes = g.clients_by_country("Spain")
-
-print(clientes)
+        return [client for client in self.clients if client.country == country]
